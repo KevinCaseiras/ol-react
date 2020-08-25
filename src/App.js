6e348811c912777976eab9c7796c994ff64289ea
Kevin Caseiras <kcaseiras@gmail.com>
2020-08-25 11:59:18 -0400

    Misc changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/calendars">Calendars</Link>
+              </li>
+              <li>
+                <Link to="/agendas">Agendas</Link>
+              </li>
+              <li>
+                <Link to="/bills">Bills</Link>
+              </li>
+              <li>
+                <Link to="/laws">Laws</Link>
+              </li>
+              <li>
+                <Link to="/transcripts">Transcripts</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <hr/>
+
+          <Switch>
+            <Route path="/calendars">
+              <Calendars/>
+            </Route>
+            <Route path="/agendas">
+              <Agendas/>
+            </Route>
+            <Route path="/bills">
+              <Bills/>
+            </Route>
+            <Route path="/laws">
+              <Laws/>
+            </Route>
+            <Route path="/transcripts">
+              <Transcripts/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+function Calendars() {
+  return <h2>Calendars</h2>;
+}
+
+function Agendas() {
+  return <h2>Agendas</h2>;
+}
+
+function Bills() {
+  return <h2>Bills</h2>;
+}
+
+function Laws() {
+  return <h2>Laws</h2>;
+}
+
+function Transcripts() {
+  return (
+    <div>
+      <h2>Transcripts</h2>
+      <TranscriptSearch/>
+    </div>
+  );
+}
+
+class TranscriptSearch extends React.Component {
+  render() {
+    return (
+      <div>transcript search form</div>
+    );
+  }
+}
